f736c2c37d079b4c2e51db086b7b26c3a96bd0ed
jpdga-shizuoka <56290481+jpdga-shizuoka@users.noreply.github.com>
2020-04-07 17:09:40 +0900

    add an about-page

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-about',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private location: Location,
+  ) { }
 
   ngOnInit(): void {
   }
 
+  back() {
+    this.location.back();
+  }
+
+  get title() {
+    return '当サイトについて';
+  }
 }
