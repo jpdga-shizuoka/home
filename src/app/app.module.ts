213747aee6a07b3c8cd930e382b3e600e0fb9aba
jpdga-shizuoka <56290481+jpdga-shizuoka@users.noreply.github.com>
2020-04-07 09:19:50 +0900

    renew with angular9

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,32 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FlexLayoutModule } from '@angular/flex-layout';
-import { MarkdownModule } from 'ngx-markdown';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MaterialModule } from './material.module';
-import { PolicyComponent } from './policy/policy.component';
-import { HomeComponent } from './home/home.component';
-import { FacebookPageComponent } from './facebook-page/facebook-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    PolicyComponent,
-    HomeComponent,
-    FacebookPageComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule,
-    MaterialModule,
-    FlexLayoutModule,
-    AppRoutingModule,
-    HttpClientModule,
-    MarkdownModule.forRoot(),
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
