01907d613477d6291492646f76b9d583f95ae0d6
jpdga-shizuoka <56290481+jpdga-shizuoka@users.noreply.github.com>
2020-04-08 21:01:12 +0900

    update view of videos component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -18,8 +18,12 @@ const routes: Routes = [
                   こまかいルールなんて知らなくても大丈夫!</p>
                   <p>Next: <a href="videos/letsplaydiscgolf">ディスクゴルフ入門</a></p>
                   <p>(提供:
-                    <a target="_blank" rel="noopener" href="https://www.youtube.com/channel/UCKRGfUYpPhad8UMv0n_RLOg">JPDGA Movie</a>,
-                    <a target="_blank" rel="noopener" href="https://www.youtube.com/channel/UCgwRgfuy5_KxhRknzu7qqyg">CHUMSomotesando</a>)
+                    <a target="_blank" rel="noopener" href="https://www.youtube.com/channel/UCKRGfUYpPhad8UMv0n_RLOg">
+                      JPDGA Movie
+                    </a>,
+                    <a target="_blank" rel="noopener" href="https://www.youtube.com/channel/UCgwRgfuy5_KxhRknzu7qqyg">
+                      CHUMSomotesando
+                    </a>)
                   </p>`,
     category: 'whatsdiscgolf',
   }},
@@ -29,15 +33,17 @@ const routes: Routes = [
                   これを見たらもうコースデビューするしか無い!</p>
                   <p>Next: <a href="videos/thisisdiscgolf">これがディスクゴルフだ</a></p>
                   <p>(提供:
-                    <a target="_blank" rel="noopener" href="https://www.youtube.com/channel/UCIT1UG20GhW2E23fPnM0Jtg">INNOVA JAPAN Disc Golf TV</a>)
+                    <a target="_blank" rel="noopener" href="https://www.youtube.com/channel/UCIT1UG20GhW2E23fPnM0Jtg">
+                      INNOVA JAPAN Disc Golf TV
+                    </a>)
                   </p>`,
     category: 'letsplaydiscgolf',
   }},
   { path: 'videos/thisisdiscgolf', component: VideosComponent, data: {
     title: "これがディスクゴルフだ",
-    description: `<p>世界的トップアスリートによる紹介ビデオだよ!
-                  海外では多くのプロ選手が競技として戦っているんだ.
-                  見てるだけでシビレル〜</p>`,
+    description: `<p>コースデビューしてもしなくても, 世界のトップアスリートのプレーをあらためて見てみよう!</p>
+                  <p>オリンピック競技候補に上がるのも納得のプレーばかり!
+                  もう信じられな〜い😱</p>`,
     category: 'thisisdiscgolf',
   }},
 ];
