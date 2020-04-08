59d5a7294d651c01fafdc3217d1f728f66f1a2fa
jpdga-shizuoka <56290481+jpdga-shizuoka@users.noreply.github.com>
2020-04-08 12:44:11 +0900

    add a video lib for waht's disc golf

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,17 +8,29 @@ import { VideosComponent } from './videos/videos.component';
 const routes: Routes = [
   { path: '', component: DashboardComponent },
   { path: 'about', component: AboutComponent },
+  { path: 'videos/whatsdiscgolf', component: VideosComponent, data: {
+    title: "ディスクゴルフってなに?",
+    description: `誰にでもできるお遊びでもあり, そして競技としても楽しめる奥が深〜いスポーツだよ!
+                  でも最初はみんな初心者, まずはビデオを見て様子を見てみよう.
+                  おもしろそうって思ってくれました?
+                  そしたら, さっそくコースに出かけよう!
+                  周りの人にぶつけないように気を付けさえすれば,
+                  こまかいルールなんて知らなくても大丈夫!
+                  (提供: JPDGA, Innova Japan, CHUMS)`,
+    category: 'whatsdiscgolf',
+  }},
   { path: 'videos/letsplaydiscgolf', component: VideosComponent, data: {
     title: "ディスクゴルフ入門",
     description: `日本チャンピオン菊地哲也プロによる入門チャンネルをチェックしよう!
-                  これを見たらもうコースデビューするしか無い! (Innova Japan提供)`,
+                  これを見たらもうコースデビューするしか無い!
+                  (提供: Innova Japan)`,
     category: 'letsplaydiscgolf',
   }},
   { path: 'videos/thisisdiscgolf', component: VideosComponent, data: {
     title: "これがディスクゴルフだ",
     description: `世界的トップアスリートによる紹介ビデオだよ!
                   海外では多くのプロ選手が競技として戦っているんだ.
-                  ルールなんて分からなくてもいい, 見てるだけでシビレル〜`,
+                  見てるだけでシビレル〜`,
     category: 'thisisdiscgolf',
   }},
 ];
--- src/app/videos.service.ts
+++ src/app/videos.service.ts
@@ -3,6 +3,7 @@ import { Observable, of } from 'rxjs';
 
 import { VIDEO } from './models';
 
+import WHATS_DISCGOLF from '../assets/models/whats-discgolf.json';
 import LETS_PLAY_DISCGOLF from '../assets/models/lets-play-discgolf.json';
 import THIS_IS_DISCGOLF from '../assets/models/this-is-discgolf.json';
 
@@ -15,6 +16,8 @@ export class VideosService {
 
   getVideos(category: string): Observable<VIDEO[]> {
     switch(category) {
+      case 'whatsdiscgolf':
+        return of(WHATS_DISCGOLF);
       case 'letsplaydiscgolf':
         return of(LETS_PLAY_DISCGOLF);
       case 'thisisdiscgolf':
